c3bff3aaacece1d930c985e78448c80116701cd0
thanhlong0728 <thanhlong0728@gmail.com>
2024-10-04 14:19:42 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -5,9 +5,11 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Dimensions,
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -24,6 +26,10 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import {FFmpegKit, FFprobeKit, FFmpegSession} from 'ffmpeg-kit-react-native';
+import RNFS from 'react-native-fs';
+import Video from 'react-native-video';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -59,8 +65,49 @@ function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: 'black',
+    flex: 1,
   };
+
+  const [videoWartermark, setVideoWartermark] = useState('');
+
+  const getDestinationPath = async (fileName: string) => {
+    const destinationPath = RNFS.CachesDirectoryPath + `/${fileName}`;
+    const exists = await RNFS.exists(destinationPath);
+    if (exists) {
+      await RNFS.unlink(destinationPath);
+    }
+    return destinationPath;
+  };
+
+  const addVideoLogo = async () => {
+    const videoUrl =
+      'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4';
+    let thumbnailName = 'thumbnail-' + new Date().getTime() + '.mp4';
+    let destinationPath = await getDestinationPath(thumbnailName);
+    let logo =
+      'https://firebasestorage.googleapis.com/v0/b/appkousei.appspot.com/o/images%2Fic_shareabill.png?alt=media&token=6bbaef3e-105d-45df-b6a2-e173a860c822';
+
+    let overlayCommand = `-filter_complex overlay=x=(main_w-overlay_w)/2:y=(main_h-overlay_h)/2`;
+    let ffmpegCommand = `-i ${videoUrl} -v quiet -i ${logo}  ${overlayCommand} -preset ultrafast ${destinationPath}`;
+
+    try {
+      await FFmpegKit.execute(ffmpegCommand);
+      console.log('destinationPath::::', destinationPath);
+      if (destinationPath) {
+        setVideoWartermark(destinationPath);
+      }
+
+      return destinationPath;
+    } catch (error) {
+      console.log('error::', error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    addVideoLogo();
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -71,26 +118,16 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        <Video
+          // Can be a URL or a local file.
+          source={{uri: videoWartermark}}
+          repeat
+          controls
+          // Store reference
+          // Callback when remote video is buffering
+          // Callback when video cannot be loaded
+          style={{width: Dimensions.get('window').width, height: 400}}
+        />
       </ScrollView>
     </SafeAreaView>
   );
